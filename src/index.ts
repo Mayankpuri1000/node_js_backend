12a428c7b09a742952bf0149d73db17b560a0be3
Mayankpuri1000 <mayankpuri1000@gmail.com>
2026-09-16 16:15:23 +0530

    cors and other basic middlewares

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,27 @@ import express, {Request, Response} from "express";
 import dotenv from "dotenv"
 dotenv.config();
 
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const port = process.env.PORT
+
+// CORS Configuration
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS']
+  }),
+);
+
+// Basic middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.use(cookieParser());
 
 app.get("/", (req: Request, res: Response) => {
     res.send("<h1>Hello Backend</h1>")
